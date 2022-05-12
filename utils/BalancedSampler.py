1bfbf0ebf277c795f61d5f16989e5742c659ed78
Racemuis <chiara.thoeni@ru.nl>
2022-05-12 11:01:57 +0200

    Add prework for PatchExtractor

---
+++ utils/BalancedSampler.py
@@ -0,0 +1,19 @@
+import numpy as np
+import random
+
+class BalancedSampler:
+
+    def __init__(self, lbls, msks):
+        # pre calculate the positive and negative indices
+        lbls = np.squeeze(lbls, 3)
+        msks = np.squeeze(msks, 3)
+        self.p_idxs = np.asarray(np.where(lbls > 0)).T.tolist()
+        self.n_idxs = np.asarray(np.where((msks > 0) & ~(lbls > 0))).T.tolist()
+
+    def generate_sample_locations(self, batch_size):
+        # generate locations half from the positive set and half from the negative set
+        p_locations = random.sample(self.p_idxs, batch_size // 2)
+        n_locations = random.sample(self.n_idxs, batch_size - batch_size // 2)
+        locations = np.vstack([p_locations, n_locations])
+        return locations
+
---
+++ utils/BatchCreator.py
@@ -0,0 +1,74 @@
+
+import numpy as np
+from BalancedSampler import BalancedSampler
+from PatchExtractor import PatchExtractor
+
+from tensorflow.keras.utils import to_categorical
+
+def pad(images, patch_size):
+        """
+        images: list of images (numpy arrays)
+        returns a padded version of the images, with a border of half the patch_size around each image
+        """
+        half_py, half_px = [p//2 for p in patch_size]
+        paddings = ((0, 0), (half_py, half_py), (half_px, half_px), (0, 0))
+        return np.pad(np.array(images), pad_width=paddings, mode='constant') 
+
+class BatchCreator:
+    
+    def __init__(self, patch_extractor, dataset, border_pad_size):
+        self.patch_extractor = patch_extractor
+
+        # the images are padded with half the patch-size around the border
+        # this way, we don't risk extracting patches from the border, that extend beyond the original image
+        self.imgs = pad(dataset.imgs, border_pad_size)
+        self.lbls = pad(np.expand_dims(dataset.lbls, 3), border_pad_size)
+        self.msks = pad(np.expand_dims(dataset.msks, 3), border_pad_size)
+        self.patch_location_sampler = BalancedSampler(self.lbls, self.msks)
+       
+    
+    def create_batch(self, batch_size):
+        '''
+        returns a class-balanced array of patches (x) with corresponding labels (y) in one-hot structure
+        '''
+        x_data = np.zeros((batch_size, *self.patch_extractor.patch_size, 3))
+        y_data = np.zeros((batch_size, 1, 1, 2))  # one-hot encoding
+        locations = self.patch_location_sampler.generate_sample_locations(batch_size)
+        
+        for i, l in enumerate(locations):
+            index, y, x = l
+            x_data[i], y_full = self.patch_extractor.get_patch(self.imgs[index], self.lbls[index], (y,x))
+            label = y_full[y_full.shape[0]//2, y_full.shape[1]//2].squeeze()%254
+            y_data[i] = np.array([1-label, label])
+            
+        return x_data, y_data
+        
+    def get_generator(self, batch_size):
+        '''returns a generator that will yield batches infinitely'''
+        while True:
+            yield self.create_batch(batch_size)
+
+
+class UNetBatchCreator(BatchCreator):
+
+    def __init__(self, patch_extractor, dataset, border_pad_size):
+        super(UNetBatchCreator, self).__init__(patch_extractor, dataset,
+                                                    border_pad_size)
+
+        self.patch_location_sampler = BalancedSampler(self.lbls, self.msks)
+    
+    def create_batch(self, batch_size):
+        '''
+        returns a batch of image patches (x) with corresponding label patches (y) in one-hot structure
+        '''
+        x_data = np.zeros((batch_size, *self.patch_extractor.patch_size, 3))
+        y_data = np.zeros((batch_size, *self.patch_extractor.patch_size, 2))  # one-hot encoding
+
+        locations = self.patch_location_sampler.generate_sample_locations(batch_size)
+        
+        for i, l in enumerate(locations):
+            index, y, x = l
+            x_data[i], y_out = self.patch_extractor.get_patch(self.imgs[index], self.lbls[index], (y,x))
+            y_data[i] = to_categorical(y_out)
+                    
+        return x_data, y_data
